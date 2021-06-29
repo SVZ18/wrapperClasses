a4f88c7159f7df796668c36728294936d42d61e0
Sandija Veze <sandijaveze@inbox.lv>
2021-06-29 21:59:21 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String a = "6";
+	    String b = "8";
+	    double a6 = Double.parseDouble(a);
+	    double b8 = Double.parseDouble(b);
+	    		System.out.println(Double.sum(a6, b8));
+
+    }
+}
